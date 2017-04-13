7aba2a5510e1a3dbbcdb84b489397e72f2ae61d3
github username <github email address>
2017-04-13 16:59:45 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if (num <= 1)
+    return false
+  elsif (num > 1)
+    range = Math.sqrt(num).to_i
+    for i in (2..range)
+      return false if (num % i == 0)
+    end
+    return true
+  end
+end
